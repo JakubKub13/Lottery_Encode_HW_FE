7d9418557ec6f2e831afbe92d3d2be062539c8cd
JK1331 <jakubkubala3@gmail.com>
2022-10-24 16:45:31 +0200

    Adding styles

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import currentEpoch from '../../helpers/currentEpoch'
+import { LotteryService } from '../lottery.service'
+
+declare var window: any
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit {
+  lotteryContractAddress: string
+  isBettingWindowOpen: Boolean
+  isLotteryRollAvailable: Boolean
+  isAttemptingLotteryRoll: Boolean
+  latestLotteryWinner: string
+  lotteryTimeWindowCountdown: string
+  closingTimeString: string
+  closingEpochInSeconds: number
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private contractsService: LotteryService) {
+    this.lotteryContractAddress = ''
+    this.isBettingWindowOpen = false
+    this.isLotteryRollAvailable = false
+    this.isAttemptingLotteryRoll = false
+    this.latestLotteryWinner = ''
+    this.lotteryTimeWindowCountdown = ''
+    this.closingEpochInSeconds = 0
+    this.closingTimeString = ''
   }
 
+  async ngOnInit(): Promise<void> {
+    this.lotteryContractAddress = this.contractsService.lotteryContractAddress
+    this.isBettingWindowOpen = await this.contractsService.isBettingWindowOpen()
+    this.isLotteryRollAvailable = await this.contractsService.isLotteryRollAvailable()
+    this.latestLotteryWinner = await this.contractsService.getLatestLotteryWinner()
+    this.closingEpochInSeconds = await this.contractsService.getClosingEpochTime()
+    this.closingTimeString = new Date(
+      this.closingEpochInSeconds * 1000,
+    ).toISOString()
+  }
+
+  async rollLottery() {
+    this.isAttemptingLotteryRoll = true
+    const { ethereum } = window
+    const isLotteryRollSuccess = await this.contractsService.rollLottery(
+      ethereum,
+    )
+    if (isLotteryRollSuccess) {
+      window.alert('Lottery roll successful!')
+      await this.ngOnInit()
+    }
+    this.isAttemptingLotteryRoll = false
+  }
 }
