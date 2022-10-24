12bd2fe8d60f386b5c4659ac7acdf074aded21a3
JK1331 <jakubkubala3@gmail.com>
2022-10-24 18:26:11 +0200

    Debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { LandingComponent } from './landing/landing.component';
 import { WagerComponent } from './wager/wager.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,7 @@ import { WagerComponent } from './wager/wager.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
