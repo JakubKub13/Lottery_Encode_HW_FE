558b6ecf0665833f56e4ab12464414057f43f86d
JK1331 <jakubkubala3@gmail.com>
2022-10-24 16:07:00 +0200

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
 import { WagerComponent } from './wager/wager.component';
 
